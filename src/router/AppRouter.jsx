762123f52588a3d08540e6529d82af0b5ff0589b
German Salinas <germansalinas.fce@gmail.com>
2023-10-03 17:55:34 -0300

    estructura

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,4 @@
-import { Routes } from 'react-router-dom';
-import { Route } from 'react-router-dom';
+import { Routes,Route, Navigate } from 'react-router-dom';
 import Home from '../pages/Home';
 import About from '../pages/About';
 
@@ -10,8 +9,7 @@ const AppRouter = () => {
             <Routes>
                 <Route path="/home" element={<Home />} />
                 <Route path="/about" element={<About />} />
-                {/* <Route path="/about" element={<About />} />
-            <Route path="/dashboard" element={<Dashboard />} /> */}
+                <Route path="*" element={ <Navigate to={"/home"}/> } />
             </Routes>
         </div>
     )
