724de770976e069fc8d572c048e50f3aaa3cfe1e
German Salinas <germansalinas.fce@gmail.com>
2023-10-15 00:00:08 -0300

    cambio en el boton

--- src/components/Cart/CardCarrito.jsx
+++ src/components/Cart/CardCarrito.jsx
@@ -34,57 +34,57 @@ const CardCarrito = ({ publicacionesCarrito, disminuir, aumentar, eliminar }) =>
         <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'space-between' }}>
             {/* Mitad izquierda con productos agregados */}
             <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', width: '40%', ml: '5%' }}>
-            {publicacionesCarrito.map((publicacion) => (
-                <Card key={publicacion.idPublicacion} sx={{ display: 'flex', marginBottom: '20px', borderRadius: 3, height: ' 190px' }}>
-                    <CardMedia
-                        component="img"
-                        sx={{ 
-                            width: '100px', 
-                            height: '100px', 
-                            objectFit: 'contain', 
-                            margin: 'auto 10px auto 20px'
-                        }}
-                        image={publicacion.idProductoNavigation.urlImagen}
-                        alt={publicacion.idProductoNavigation.nombre}
-                    />
-                    <CardContent sx={{ flex: 1 }}>
-                        <Typography variant="h5">{publicacion.idProductoNavigation.nombre}</Typography>
-                        <Typography variant="body1" sx={{ marginTop: '5px', fontWeight:'bold'  }}>
-                         {formatPrice(publicacion.precio)}
-                        </Typography>
-                        <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
-                            <IconButton onClick={() => disminuir(publicacion.idPublicacion)}>
-                                <RemoveIcon />
-                            </IconButton>
-                            <Typography>{publicacion.cantidad}</Typography>
-                            <IconButton onClick={() => aumentar(publicacion.idPublicacion)}>
-                                <AddIcon />
-                            </IconButton>
-                            <IconButton
-                                edge="end"
-                                sx={{ marginLeft: 'auto', color: 'red' }}
-                                onClick={() => eliminar(publicacion.idPublicacion)}
-                            >
-                                <DeleteIcon />
-                            </IconButton>
-                        </Box>
-                    </CardContent>
-                </Card>
-            ))}
-        </Box>
+                {publicacionesCarrito.map((publicacion) => (
+                    <Card key={publicacion.idPublicacion} sx={{ display: 'flex', marginBottom: '20px', borderRadius: 3, height: ' 190px' }}>
+                        <CardMedia
+                            component="img"
+                            sx={{
+                                width: '100px',
+                                height: '100px',
+                                objectFit: 'contain',
+                                margin: 'auto 10px auto 20px'
+                            }}
+                            image={publicacion.idProductoNavigation.urlImagen}
+                            alt={publicacion.idProductoNavigation.nombre}
+                        />
+                        <CardContent sx={{ flex: 1 }}>
+                            <Typography variant="h5">{publicacion.idProductoNavigation.nombre}</Typography>
+                            <Typography variant="body1" sx={{ marginTop: '5px', fontWeight: 'bold' }}>
+                                {formatPrice(publicacion.precio)}
+                            </Typography>
+                            <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '10px' }}>
+                                <IconButton onClick={() => disminuir(publicacion.idPublicacion)}>
+                                    <RemoveIcon />
+                                </IconButton>
+                                <Typography>{publicacion.cantidad}</Typography>
+                                <IconButton onClick={() => aumentar(publicacion.idPublicacion)}>
+                                    <AddIcon />
+                                </IconButton>
+                                <IconButton
+                                    edge="end"
+                                    sx={{ marginLeft: 'auto', color: 'red' }}
+                                    onClick={() => eliminar(publicacion.idPublicacion)}
+                                >
+                                    <DeleteIcon />
+                                </IconButton>
+                            </Box>
+                        </CardContent>
+                    </Card>
+                ))}
+            </Box>
 
             {/* Mitad derecha con detalles del producto agregado */}
-            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', width: '40%', mr: '10%'}}>
+            <Box sx={{ display: 'flex', flexDirection: 'column', alignItems: 'left', justifyContent: 'left', width: '40%', mr: '10%' }}>
                 <Card sx={{ display: 'flex', flexDirection: 'column', marginBottom: '15px', padding: 3, borderRadius: 3 }}>
-                    <Typography variant="h5" sx={{ marginBottom: '20px',fontWeight: 'bold' }}>Detalles del Pedido</Typography>
-                    <Divider sx={{ marginBottom: '20px', mt:1 }} />
+                    <Typography variant="h5" sx={{ marginBottom: '20px', fontWeight: 'bold' }}>Detalles del Pedido</Typography>
+                    <Divider sx={{ marginBottom: '20px', mt: 1 }} />
                     {publicacionesCarrito.map((publicacion) => (
-                        <Box key={publicacion.idPublicacion} sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px', marginTop:'10px' }}>
+                        <Box key={publicacion.idPublicacion} sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: '10px', marginTop: '10px' }}>
                             <Typography variant="body1" sx={{ fontSize: '1.1em' }}>{publicacion.idProductoNavigation.nombre} ({publicacion.cantidad})</Typography>
                             <Typography variant="body1" sx={{ fontSize: '1.1em' }}> {formatPrice(publicacion.precio * publicacion.cantidad)}</Typography>
                         </Box>
                     ))}
-                                        <Divider sx={{mt:5 }} />
+                    <Divider sx={{ mt: 5 }} />
 
                     <Box sx={{ display: 'flex', justifyContent: 'space-between', marginTop: '50px' }}>
                         <Typography variant="h5" sx={{ fontWeight: 'bold' }}>Total</Typography>
